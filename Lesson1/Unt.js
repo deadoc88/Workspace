338492861a1cafa3811977c568c0d11e276519c7
deadoc88 <49363157+deadoc88@users.noreply.github.com>
2019-04-07 13:18:09 +0300

    Update Unt.js

--- Lesson1/Unt.js
+++ Lesson1/Unt.js
@@ -1,12 +1,1 @@
 
-const smallestDivisor = (n) => {
-  const iter = (num, acc) => {
-    if (num === 1) {
-      return acc;
-    }
-    return iter(num - 1, num * acc);
-  }
-  return iter (n, 2);
-  // END
-};
-console.log(smallestDivisor(5));
